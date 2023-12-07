2eba7e568a6ec01d608ac514605b1819da451ab3
ÖZGE DİNÇER <ozgedincer@posta.mu.edu.tr>
2023-12-07 19:43:34 +0300

    Proje dosyası ekle.

---
+++ Presentation/Week_11.API/Models/GetBankAccountDataResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Week_11.API.Models
+{
+    public class GetBankAccountDataResponseModel
+    {
+        //[Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        //[Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        //[Required(ErrorMessage = "Balance is required")]
+        public decimal Balance { get; set; }
+    }
+}
